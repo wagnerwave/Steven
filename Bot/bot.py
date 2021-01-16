287b2455bdf93ec7e35e46bd266edce47446f3e1
wagnerwave <alexandre1.wagner@epitech.eu>
2021-01-17 00:49:45 +0100

    ADD  - je continue a tester l'api + d'autre truck

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import websocket, json
+import pprint
 
 SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@trade"
 
@@ -9,19 +10,25 @@ def on_open(ws):
 
 def on_close(ws):
     print('########### CLOSE CONNECTION ###########')
-
+ 
 def on_error(ws, error):
     print(error)
 
 def on_message(ws, message):
     print('received message:', message)
     json_message = json.loads(message)
-    print(json_message)
-    candle = json_message['k']
-    close = candle['x']
-    is_close_candle = candle['c']
-    if is_close_candle:
-        print("candle closed at {}".format(close))
+    pprint.pprint(json_message)
+
+    # candle = json_message['k']
+    
+    # is_close_candle = candle['c']
+    # close = candle['x']
+    # if is_close_candle:
+    #     print("candle closed at {}".format(close))
+    #     closes.append(float(close))
+    #     print("closes")
+    #     print(closes)
+
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
