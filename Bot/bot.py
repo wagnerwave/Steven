855147efa1d0775b8eb38d24621e7a556fccbe66
wagnerwave <alexandre1.wagner@epitech.eu>
2021-01-19 13:08:11 +0100

    ADD - Correction algo

--- Bot/bot.py
+++ Bot/bot.py
@@ -10,6 +10,7 @@ RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'BTCUSD'
 TRADE_QUATITY = 0.05
 
+global in_position
 closes = []
 
 def on_open(ws):
@@ -22,40 +23,37 @@ def on_error(ws, error):
     print(error)
 
 def on_message(ws, message):
-    in_position = False
     print('received message')
     json_message = json.loads(message)
     pprint.pprint(json_message)
-
     candle = json_message['k']
-    
-    is_close_candle = candle['c']
+    candle_price_close = candle['c']
     close = candle['x']
-    if is_close_candle:
-        print("candle closed at {}".format(close))
-        closes.append(float(close))
+    if close:
+        print("candle closed at {}".format(candle_price_close))
+        closes.append(float(candle_price_close))
         if len(closes) > RSI_PERIOD:
             np_closes = numpy.array(closes)
             rsi = talib.RSI(np_closes, RSI_PERIOD)
-            # print("all rsis calculated so far")
-            # print(rsi)
             last_rsi = rsi[-1]
+            print("lenght of rsi -> ", len(rsi))
+            print("rsi ->", rsi)
             print("the current rsi is {}".format(last_rsi))
             if last_rsi > RSI_OVERBOUGHT:
                 if in_position:
                     print("STEVEN SAY : You need to sell, right now !")
                     in_position = False
                 else:   
-                    print("STEVEN SAY : Nothing to do.")
+                    print("STEVEN SAY : It is overbought, you have already sell, Nothing to do.")
             if last_rsi < RSI_OVERSOLD:
                 if in_position:
-                    print("STEVEN SAY : Nothing to do.")
+                    print("STEVEN SAY : It is oversold, you have already buy, Nothing to do.")
                 else:
                     print("STEVEN SAY : You need to buy, right now !")
                     in_position = True
-
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
     ws.run_forever()
+g
