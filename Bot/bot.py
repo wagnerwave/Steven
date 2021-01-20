391316c19a156e169864431a8778c040ff27ecb7
wagnerwave <alexandre1.wagner@epitech.eu>
2021-01-20 10:29:48 +0100

    ADD - Correction bollinger work need to impletment order and the choice

--- Bot/bot.py
+++ Bot/bot.py
@@ -19,22 +19,14 @@ def RSI_strat(last_rsi):
     if last_rsi > RSI_OVERBOUGHT:
         if in_position:
             print(in_position)
-            print("STEVEN SAY : You need to sell, right now !")
-            in_position = False
             return -1
         else:   
-            print("STEVEN SAY : It is overbought, you have already sell, Nothing to do.")
             return 0
     if last_rsi < RSI_OVERSOLD:
         if in_position:
-            print(in_position)
-            print("STEVEN SAY : It is oversold, you have already buy, Nothing to do.")
             return 0
         else:
-            print("STEVEN SAY : You need to buy, right now !")
-            in_position = True
             return 1
-    print("last rsi is not under overbourh or over oversold")
     return 0
 
 def sma(closes_candles, n):
@@ -80,6 +72,20 @@ def bollinger_strat(candles, n):
 
 #################################################################
 
+def choice(ret_value_rsi, ret_value_bollinger):
+    total = ret_value_bollinger + ret_value_rsi
+    if (total < 1):
+        in_position = True
+        print("STEVEN SAY : You need to buy, right now !")
+    if (total > 0):
+        print("STEVEN SAY : You need to sell, right now !")
+        in_position = False
+    else:
+        print("STEVEN SAY : nothing to do")
+
+
+################################################################
+
 def on_open(ws):
     print('########### OPEN CONNECTION ###########')
 
@@ -90,9 +96,9 @@ def on_error(ws, error):
     print(error)
 
 def on_message(ws, message):
-    print('received message')
+    #print('received message')
     json_message = json.loads(message)
-    pprint.pprint(json_message)
+    #pprint.pprint(json_message)
     candle = json_message['k']
     candle_price_close = candle['c']
     close = candle['x']
@@ -107,7 +113,7 @@ def on_message(ws, message):
             last_rsi = rsi[-1]
             print("the current rsi is {}".format(last_rsi))
             ret_value_rsi = RSI_strat(last_rsi)
-            ret_value_bollinger = bollinger_strat(rsi, len(rsi))
+            ret_value_bollinger = bollinger_strat(closes, len(closes))
             print("##################")
             print("deja achete {}".format(in_position))
             print("ret_value_rsi ->", ret_value_rsi)
