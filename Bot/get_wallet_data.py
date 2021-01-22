b6779de49b595f1ebe13b1a69f01af70909460fe
tristan <tristan.le-bars@epitech.eu>
2021-01-23 00:15:38 +0100

    ADD - adding functions to get wallet data

---
+++ Bot/get_wallet_data.py
@@ -0,0 +1,23 @@
+from binance.client import Client
+
+client = Client('API_KEY', 'SECRET_KEY')
+
+def getWalletBTC():
+    BTC = client.get_asset_balance(asset='BTC')
+    print(BTC)
+
+def getWalletETH():
+    ETH = client.get_asset_balance(asset='ETH')
+    print(ETH)
+
+def getWalletUSDC():
+    USDC = client.get_asset_balance(asset='USDC')
+    print(USDC)
+
+def getAllWallet():
+    getWalletBTC()
+    getWalletETH()
+    getWalletUSDC()
+
+if __name__ == "__main__":
+    getAllWallet()
