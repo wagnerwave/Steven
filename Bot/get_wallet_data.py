b7619c75670cd6b7ec4fe8362745ca19848d54a9
Tristan Le Bars <tristan.le-bars@epitech.eu>
2021-01-24 22:32:43 +0100

    ADD - adding monero in get_wallet_data.py + adding risk_manager.py file

--- Bot/get_wallet_data.py
+++ Bot/get_wallet_data.py
@@ -14,10 +14,15 @@ def getWalletUSDC():
     USDC = client.get_asset_balance(asset='USDC')
     print(USDC)
 
+def getWalletXMR():
+    XMR = client.get_asset_balance(asset='XMR')
+    print(XMR)
+
 def getAllWallet():
     getWalletBTC()
     getWalletETH()
     getWalletUSDC()
+    getWalletXMR()
 
 if __name__ == "__main__":
     getAllWallet()
