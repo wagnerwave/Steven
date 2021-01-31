ae5a1c23334585c000a917c3dffe519a25964060
wagnerwave <alexandre1.wagner@epitech.eu>
2021-01-31 19:26:22 +0100

    ADD - Changement du main, parsing et help

--- Bot/main.py
+++ Bot/main.py
@@ -1,123 +1,63 @@
 #!/usr/bin/env python3
 
-from binance.client import Client
-from binance.enums import *
-from rsi import RSI_strat
-from bollinger import bollinger_strat
-from config import API_KEY, API_SECRET_KEY
+import sys
+from bot import Bot
 
-import websocket, json, pprint, numpy, talib, math
-import datetime
+def Usage():
+    print("python3 main.py [Period] [Symbol] [Quantity]")
+    print()
+    print("Exemple : python3 main.py short BTC 0.001")
+    print()
+    print("Period : Short - Medium - Long")
+    print("\t Short : 1 hour")
+    print("\t Medium : 1 week")
+    print("\t Long : 1 mouth")
+    print()
+    print("Symbol : BTC - ETH - XMR")
+    print("\t BTC : Bitcoin")
+    print("\t ETH : Etherium ")
+    print("\t XMR : Monero")
+    print()
+    print("Quatity : the quantities of money for your trading bot")
+    print("\t Exemple : 0.650")
 
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
-client = Client(API_KEY, API_SECRET_KEY, tld='us')
-
-RSI_PERIOD = 14
-RSI_OVERBOUGHT = 65
-RSI_OVERSOLD = 40
-TRADE_SYMBOL = 'ETHUSD'
-TRADE_QUATITY = 0.001
-
-closes = []
-in_position = False
-
-#################################################################
-
-def buy_crypto(symbol, quatity):
-    try:
-        order = client.create_order(symbol=symbol, side=SIDE_BUY, type=ORDER_TYPE_MARKET, quatity=quatity)
-        print("Order complete: {}".format(order))
-    except Exception as e:
-        print("Error to buy: {}".format(e))
-        return False
-    return True
-
-def sell_crypto(symbol, quatity):
-    try:
-        order = client.create_order(symbol=symbol, side=SIDE_SELL, type=ORDER_TYPE_MARKET, quatity=quatity)
-        print("Order complete: {}".format(order))
-    except Exception as e:
-        print("Error to sell: {}".format(e))
-        return False
-    return True
-
-def choice(ret_value_rsi, ret_value_bollinger):
-    global in_position
-    total = ret_value_bollinger + ret_value_rsi
-    if (total != 0):
-        if (total == 1):
-                print("STEVEN SAY : nothing to do")
-        if (total > 1):
-            if in_position == False:
-                print("STEVEN SAY : I'm gonna buy", TRADE_SYMBOL, " for ", TRADE_QUATITY," quantities!")
-                buy_success = buy_crypto(TRADE_SYMBOL, TRADE_QUATITY)
-                if (buy_success):
-                    in_position = True
-                else:
-                    in_position = False
-                    print("BIG ERROR: cannot buy !!!!")
-                    exit(84) 
-            else:
-                print("STEVEN SAY : nothing to do")
-        if (total < 0):
-            if in_position == True:
-                print("STEVEN SAY : I'm gonna sell", TRADE_SYMBOL, " for ", TRADE_QUATITY," quantities!")
-                sell_success = sell_crypto(TRADE_SYMBOL, TRADE_QUATITY)
-                if (sell_success):
-                    in_position = False
-                else:
-                    in_position = True
-                    print("BIG ERROR: cannot sell !!!!")
-                    exit(84) 
-            else:
-                print("STEVEN SAY : nothing to do")
+def parsing_period(arg):
+    if (arg == "Short" or arg == "short" or arg == "SHORT" or arg == 'S' or arg == 's'):
+        return "short"
+    elif (arg == "Medium" or arg == "medium" or arg == "MEDIUM" or arg == 'M' or arg == 'm'):
+        return "medium"
+    elif (arg == "Long" or arg == "long" or arg == "LONG" or arg == 'L' or arg == 'l'):
+        return "long"
     else:
-        print("STEVEN SAY : nothing to do")
+        Usage()
+        exit(1)
 
+def parsing_symbol(arg):
+    if (arg == "BTC" or arg == "btc"):
+        return "BTCUSD"
+    elif (arg == "ETH" or arg == "eth"):
+        return "ETHUSD"
+    elif (arg == "XMR" or arg == "xmr"):
+        return "XMR"
+    else:
+        Usage()
+        exit(1)
 
-################################################################
-
-def on_open(ws):
-    print('########### OPEN CONNECTION ###########')
-
-def on_close(ws):
-    print('########### CLOSE CONNECTION ###########')
- 
-def on_error(ws, error):
-    print(error)
-
-def on_message(ws, message):
-    global in_position
-    #print('received message')
-    json_message = json.loads(message)
-    #pprint.pprint(json_message)
-    candle = json_message['k']
-    candle_price_close = candle['c']
-    close = candle['x']
-    if close:
-        print("candle closed at {}".format(candle_price_close))
-        closes.append(float(candle_price_close))
-        print(len(closes))
-        if len(closes) > RSI_PERIOD:
-            np_closes = numpy.array(closes)
-            rsi = talib.RSI(np_closes, RSI_PERIOD)
-            print("##################")            
-            print(rsi)
-            last_rsi = rsi[-1]
-            print("the current rsi is {}".format(last_rsi))
-            ret_value_rsi = RSI_strat(last_rsi, in_position)
-            ret_value_bollinger = bollinger_strat(closes, len(closes))
-            print("##################")
-            print("deja achete {}".format(in_position))
-            print("ret_value_rsi ->", ret_value_rsi)
-            print("ret_value_bollinger ->", ret_value_bollinger)
-            print("##################")
-            print(str(datetime.datetime.now()))
-            choice(ret_value_rsi ,ret_value_bollinger)
-            print("##################")
+def parsing_quantity(arg):
+    try:
+        return float(arg)
+    except:
+        Usage()
+        exit(1)
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
-    ws.run_forever()
+    if (len(sys.argv) == 4):
+        period = parsing_period(sys.argv[1])
+        symbol = parsing_symbol(sys.argv[2])
+        quantity = parsing_quantity(sys.argv[3])
+        Steven = Bot(period=period, symbol=symbol, quatity=quantity)
+        Steven.Start()
+    else:
+        Usage()
+        exit(0)
