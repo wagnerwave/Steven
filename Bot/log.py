4f880ea4af049a6dc12ed02c779b468345a01609
wagnerwave <alexandre1.wagner@epitech.eu>
2021-01-31 22:36:00 +0100

    ADD - ajout de log

---
+++ Bot/log.py
@@ -0,0 +1,24 @@
+
+def Log_nothing_to_do():
+    print("[{}] STEVEN : Nothing to do...".format((str(datetime.datetime.now()))))
+
+def Log_buy(symbol, quantity):
+    print("[{}}] STEVEN : Buy {} For {} quantities...".format((str(datetime.datetime.now())), symbol, quantity))
+
+def Log_sell(symbol, quantity):
+    print("[{}}] STEVEN : Sell {} For {} quantities...".format((str(datetime.datetime.now())), symbol, quantity))
+
+def Log_big_error():
+    print("[{}] BIG ERROR: cannot sell !!!")
+    exit(1) 
+
+def Log_status(client):
+    BTC = client.get_asset_balance(asset='BTC')
+    ETH = client.get_asset_balance(asset='ETH')
+    USDC = client.get_asset_balance(asset='USDC')
+    XMR = client.get_asset_balance(asset='XMR')
+    print("[{}] CRYPTO STATUS : ".format((str(datetime.datetime.now())))
+    print("\t\t Bitcoin [BTC] : {}".format(BTC))
+    print("\t\t Etherium [ETH]: {}".format(ETH))
+    print("\t\t USDC [$$$] : {}".format(USDC))
+    print("\t\t XMR [Monero] : {}".format(XMR))
