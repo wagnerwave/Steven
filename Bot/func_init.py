c7d2774374a3b0c9beb06735eec7f82892012d56
wagnerwave <alexandre1.wagner@epitech.eu>
2021-02-01 19:43:15 +0100

    ADD - add file for init variable on the bot

---
+++ Bot/func_init.py
@@ -0,0 +1,25 @@
+def init_period(period):
+    if (period != "short" and period != "medium" and period != "long"):
+        print("Error: {} period is not allowed...".format(period))
+        exit(1)
+    else :
+        if (period == "short"):
+            return "1h"
+        elif (period == "medium"):
+            return "1w"
+        elif (period == "long"):
+            return "1m"
+        else:
+            print("Error: cannot find the right socket...")
+            exit(1)
+
+def init_pair_trade(symbol):
+    if (symbol == "ETH"):
+        return "ETHBUSD"
+    elif (symbol == "BTC"):
+        return "BTCBUSD"
+    elif (symbol == "XMR"):
+        return "XMRBUSD"
+    else:
+        print("Error: cannot find the right pair for trade...")
+        exit(1)
