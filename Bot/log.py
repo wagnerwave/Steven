bfff535453fe721f19bb28c3f6b65ee3fc7f413f
wagnerwave <alexandre1.wagner@epitech.eu>
2021-02-01 19:43:29 +0100

    ADD - add new log function

--- Bot/log.py
+++ Bot/log.py
@@ -1,4 +1,3 @@
-
 def Log_nothing_to_do():
     print("[{}] STEVEN : Nothing to do...".format((str(datetime.datetime.now()))))
 
@@ -21,11 +20,13 @@ def Log_status(client):
     ETH = client.get_asset_balance(asset='ETH')
     USDC = client.get_asset_balance(asset='USDC')
     XMR = client.get_asset_balance(asset='XMR')
+    XRP = client.get_asset_balance(asset='XRP')
     print("[{}] CRYPTO STATUS : ".format((str(datetime.datetime.now()))))
     print("\t\t Bitcoin [BTC] : {}".format(BTC))
     print("\t\t Etherium [ETH]: {}".format(ETH))
     print("\t\t USDC [$$$] : {}".format(USDC))
     print("\t\t XMR [Monero] : {}".format(XMR))
+    print("\t\t XRP : {}".format(XRP))
 
 def Log_candle_close(candle_price_close, symbol):
     print("[{}] {} candle close : {}".format((str(datetime.datetime.now())), symbol, candle_price_close))
