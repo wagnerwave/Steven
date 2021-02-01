df9faff60d3b37190efa3d282d9a2a0f90f1ea90
wagnerwave <alexandre1.wagner@epitech.eu>
2021-02-01 19:44:00 +0100

    ADD - changement of websocket method (use binance websocket api, now)

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,7 +1,8 @@
 from binance.client import Client
+from binance.websockets import BinanceSocketManager
 from binance.enums import *
-from getSocket import * 
-from log import log
+from func_init import init_period, init_pair_trade
+from log import *
 from rsi import RSI_strat, RSI_OVERBOUGHT, RSI_OVERSOLD
 from bollinger import bollinger_strat
 from config import API_KEY, API_SECRET_KEY
@@ -13,12 +14,25 @@ class Bot:
     def __init__(self, period, symbol, quatity):
         self._tradeSymbol = symbol
         self._tradeQuatity = quatity
-        self._period = period
+        self._pair_trade = init_pair_trade(self._tradeSymbol)
+        self._period = init_period(period)
         self._position = False
         self._client = Client(API_KEY, API_SECRET_KEY, tld='us')
         self._closes = []
         self._rsiPeriod = 14
-        self._socket = getSocket(self._period, self._tradeSymbol)
+        self._socket = BinanceSocketManager(self._client)
+
+    def _connection_test(self):
+        status = self._client.get_system_status()
+        if self._client.ping() == False:
+            print("Error: ping client doesn't work.")
+            exit(1)
+        if status['status'] != 0:
+            print("Error: System maintenance.")
+            exit(1)
+        print("#####################################")
+        print("SERVER : Connection is okay and server status is", status['msg'])
+        print("#####################################")
 
     def __buy(self):
         try:
@@ -122,46 +136,40 @@ class Bot:
         else:
             Log_nothing_to_do()
 
-    def __onOpen(ws):
-        print('########### OPEN CONNECTION ###########')
-
-    def __onClose(ws):
-        print('########### CLOSE CONNECTION ###########')
- 
-    def __onError(ws, error):
-        print(error)
-
-    def __onMessage(ws, message):
-        json_message = json.loads(message)
-        #pprint.pprint(json_message) # 
-        candle = json_message['k']
-        candle_price_close = candle['c']
-        close = candle['x']
-        if close:
-            print("candle closed at {}".format(candle_price_close))
-            self._closes.append(float(candle_price_close))
-            print(len(self._closes))
-            if len(self._closes) > self._rsiPeriod:
-                np_closes = numpy.array(closes)
-                rsi = talib.RSI(np_closes, RSI_PERIOD)
-                print("##################")            
-                #print(rsi)
-                last_rsi = rsi[-1]
-                print("the current rsi is {}".format(last_rsi))
-                ret_value_rsi = RSI_strat(last_rsi, self._position)
-                ret_value_bollinger = bollinger_strat(closes, len(closes))
-                #print("##################")
-                #print("deja achete {}".format(self._position))
-                print("ret_value_rsi ->", ret_value_rsi)
-                print("ret_value_bollinger ->", ret_value_bollinger)
-                #print("##################")
-                #self._choice(ret_value_rsi ,ret_value_bollinger)
-                self._strategie_bollinger(ret_value_bollinger)
-                Log_status(self._client)
-                print("##################")
+    def _process_message(self, message):
+        if message['e'] == 'error':
+            self._socket.close()
+            Log_socket_error()
+        else:            
+            json_message = message
+            #pprint.pprint(json_message) # 
+            candle = json_message['k']
+            candle_price_close = candle['c']
+            close = candle['x']
+            if close:
+                Log_candle_close(candle_price_close, self._tradeSymbol)
+                self._closes.append(float(candle_price_close))
+                print(len(self._closes))
+                if len(self._closes) > self._rsiPeriod:
+                    np_closes = numpy.array(closes)
+                    rsi = talib.RSI(np_closes, RSI_PERIOD)
+                    print("##################")            
+                    #print(rsi)
+                    last_rsi = rsi[-1]
+                    print("the current rsi is {}".format(last_rsi))
+                    ret_value_rsi = RSI_strat(last_rsi, self._position)
+                    ret_value_bollinger = bollinger_strat(closes, len(closes))
+                    #print("##################")
+                    #print("deja achete {}".format(self._position))
+                    print("ret_value_rsi ->", ret_value_rsi)
+                    print("ret_value_bollinger ->", ret_value_bollinger)
+                    #print("##################")
+                    #self._choice(ret_value_rsi ,ret_value_bollinger)
+                    self._strategie_bollinger(ret_value_bollinger)
+                    Log_status(self._client)
+                    print("##################")
 
     def Start(self):
-        websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(self._socket, on_open=self.__onOpen, on_close=self.__onClose, on_message=self.__onMessage)
-        ws.run_forever()
-
+        self._connection_test()
+        self._socket.start_kline_socket(self._pair_trade, self._process_message, interval=self._period)
+        self._socket.start()
