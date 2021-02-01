6389a6ba66abb538ac50c2cd1ad5d445eac4200d
wagnerwave <alexandre1.wagner@epitech.eu>
2021-02-01 20:45:37 +0100

    REMOVE - remove server because it's useless

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const WebSocket = require('ws');
-
-var binanceSocket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
-
-binanceSocket.onmessage = (event) => {
-	console.log(event.data)
-}
